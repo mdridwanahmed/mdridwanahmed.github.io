a53fde1016ee4a6e96f93b8f56cefa93f1e66a55
Md. Ridone Ahmed <34868317+r1ridwan@users.noreply.github.com>
2021-03-25 22:49:17 +0600

    upadated

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,12 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+// Preloader
+$(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(200).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
